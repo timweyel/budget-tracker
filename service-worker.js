5d107f36a40b03a2e823b0537c24a58a548c21cd
TimWeyel <tweyel@gmail.com>
2021-06-07 16:50:04 -0700

    fixed offline functionality

---
+++ service-worker.js
@@ -0,0 +1,68 @@
+const APP_PREFIX = 'budget-tracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "./server.js",
+  "./public/js/index.js",
+  "./public/js/idb.jsidb.js",
+  "./service-worker.js",
+  "./public/index.html",
+  "./public/css/styles.css",
+  "./public/icons/icon-128x128.png",
+  "./public/icons/icon-144x144.png",
+  "./public/icons/icon-152x152.png",
+  "./public/icons/icon-192x192.png",
+  "./public/icons/icon-384x384.png",
+  "./public/icons/icon-512x512.png",
+  "./public/icons/icon-72x72.png",
+  "./public/icons/icon-96x96.png",
+  "./public/css/styles.css"
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
