2042b73f06bfa4fe59e8433ecf2797d732aaca3b
TimWeyel <tweyel@gmail.com>
2021-06-08 11:31:54 -0700

    fixing service-worker.js files_to_cache yet again

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -4,21 +4,19 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "/",
-  "../../server.js",
-  "./index.js",
-  "./idb.js",
-  "./service-worker.js",
-  "../manifest.json",
-  "../index.html",
-  "../css/styles.css",
-  "../icons/icon-128x128.png",
-  "../icons/icon-144x144.png",
-  "../icons/icon-152x152.png",
-  "../icons/icon-192x192.png",
-  "../icons/icon-384x384.png",
-  "../icons/icon-512x512.png",
-  "../icons/icon-72x72.png",
-  "../icons/icon-96x96.png"
+  "/public/js/index.js",
+  "/public/js/idb.js",
+  "/public/manifest.json",
+  "/public/js/index.html",
+  "/public/css/styles.css",
+  "/public/icons/icon-128x128.png",
+  "/public/icons/icon-144x144.png",
+  "/public/icons/icon-152x152.png",
+  "/public/icons/icon-192x192.png",
+  "/public/icons/icon-384x384.png",
+  "/public/icons/icon-512x512.png",
+  "/public/icons/icon-72x72.png",
+  "/public/icons/icon-96x96.png"
 ];
 
 self.addEventListener('install', function (e) {
