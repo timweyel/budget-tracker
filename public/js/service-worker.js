19904eaf272b618e5309dff3a98afb27db738bc3
TimWeyel <tweyel@gmail.com>
2021-06-08 12:35:02 -0700

    fixed caching issue.

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -19,20 +19,6 @@ const FILES_TO_CACHE = [
   "../icons/icon-96x96.png"
 ];
 
-// "/",
-// "/index.js",
-// "/idb.js",
-// "../../public/manifest.json",
-// "../../public/index.html",
-// "../css/styles.css",
-// "../icons/icon-128x128.png",
-// "../icons/icon-144x144.png",
-// "../icons/icon-152x152.png",
-// "../icons/icon-192x192.png",
-// "../icons/icon-384x384.png",
-// "../icons/icon-512x512.png",
-// "../icons/icon-72x72.png",
-// "../icons/icon-96x96.png"
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
